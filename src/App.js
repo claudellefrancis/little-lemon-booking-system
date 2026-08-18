eb7384371792d5b92c5654a2b646d901e5625e69
claudellefrancis <claudelle.francis@umuzi.org>
2026-08-18 15:04:34 +0200

    Updated meta tags and created components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import logo from './assets/Logo.svg';
 
 function App() {
   return (
-    <div className="Homepage">
-      <header className="Homepage-header">
-        <img src={logo} className="Homepage-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="Homepage-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <header>
+      {logo && <img src={logo} alt="Little Lemon Logo" />}
+      <h1>Littel Lemon Restaurant</h1>
+    </header>
 
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+
+    <main>
+
+    </main>
+
+    <footer>
+      <div>
+        <img></img>
+
+        <div>
+          Copyright © 2026 Little Lemon, All Rights Reserved
+        </div>
+      </div>
+
+    </footer>
+
+    </>
+  )
+}
 export default App;
