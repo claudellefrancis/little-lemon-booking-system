66328e9ac89aaf457aa4dff49a85e56e8d9179c6
claudellefrancis <claudelle.francis@umuzi.org>
2026-08-22 04:32:44 +0200

    Updated styling

---
+++ little-lemon-booking-system/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Article from "./Article";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <Article />
+    </>
+  );
+}
+
+export default Homepage;
--- little-lemon-booking-system/src/components/Nav.js
+++ little-lemon-booking-system/src/components/Nav.js
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
@@ -6,12 +6,12 @@ function Nav() {
     <header>
     <nav>
       <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#menu">Menu</a></li>
-        <li><a href="#reservations">Reservations</a></li>
-        <li><a href="#order-online">Order Online</a></li>
-        <li><a href="#login">Login</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/Menu">Menu</Link></li>
+        <li><Link to="/reservations">Reservations</Link></li>
+        <li><Link to="/order-online">Order Online</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
     </header>
