273e1fa06251064bcb74a3be20bc3144a2e28f43
Claudelle <claudelle.francis@umuzi.org>
2026-08-29 19:02:57 +0200

    Fixed imports

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,8 @@ import logo from '../images/logo.png';
 
 function Header() {
   return (
-    <header>
-      <div className="logo">
-        <img src={logo} alt="Little Lemon Logo" />
-      </div>
-
-      <div>
-        <h1>Little Lemon Restaurant</h1>
-        </div>
-
+    <header className="header">
+      {logo}
       <Nav />
     </header>
   );
